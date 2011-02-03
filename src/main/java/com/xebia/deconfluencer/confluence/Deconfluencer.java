2f6cbdf7a6e3c9730b3111c40fd69a8aa7715d08
Wilfred Springer <wilfred@flotsam.nl>
2011-02-03 15:52:15 +0100

    Made sure XSLT can be reloaded.

--- src/main/java/com/xebia/deconfluencer/confluence/Deconfluencer.java
+++ src/main/java/com/xebia/deconfluencer/confluence/Deconfluencer.java
@@ -1,14 +1,42 @@
+/**
+ * Copyright (c) 2011, Wilfred Springer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the <organization>.
+ * 4. Neither the name of the <organization> nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.xebia.deconfluencer.confluence;
 
+import java.io.File;
 import java.io.InputStream;
 import javax.xml.transform.Templates;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamSource;
 import org.eclipse.jetty.server.Server;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
-import com.google.common.io.Resources;
 import com.xebia.deconfluencer.Loader;
 import com.xebia.deconfluencer.Logger;
 import com.xebia.deconfluencer.TransformingHandler;
@@ -16,6 +44,7 @@ import com.xebia.deconfluencer.loader.http.BasicAuthenticationRequestExtender;
 import com.xebia.deconfluencer.loader.http.HttpLoader;
 import com.xebia.deconfluencer.loader.http.UriBuilder;
 import com.xebia.deconfluencer.loader.neko.NekoSourceLoader;
+import com.xebia.deconfluencer.xslt.ReloadingTemplates;
 import com.xebia.deconfluencer.xslt.Transformation;
 
 public class Deconfluencer {
@@ -41,6 +70,12 @@ public class Deconfluencer {
             required = false)
     private int portNumber = DEFAULT_PORTNUMBER;
 
+    @Option(name = "-f",
+            metaVar= "FILTER",
+            usage= "The location of the XSLT filter to be applied",
+            required= true)
+    private File filter;
+
     public static final void main(String... args) throws Exception {
         Deconfluencer proxy = new Deconfluencer();
         CmdLineParser parser = new CmdLineParser(proxy);
@@ -63,8 +98,9 @@ public class Deconfluencer {
                 return result;
             }
         };
-        TransformerFactory factory = TransformerFactory.newInstance();
-        Templates templates = factory.newTemplates(new StreamSource(Resources.getResource("filter.xsl").openStream()));
+//        TransformerFactory factory = TransformerFactory.newInstance();
+//        Templates templates = factory.newTemplates(new StreamSource(Resources.getResource("filter.xsl").openStream()));
+        Templates templates = new ReloadingTemplates(filter, TransformerFactory.newInstance());
         Server server = new Server(portNumber);
         Loader<InputStream> dataLoader = new HttpLoader(builder,
                 new BasicAuthenticationRequestExtender(username, password));
